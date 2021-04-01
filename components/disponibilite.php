a03b0f3f4b0357f655ce2147a671b21269e4a0d1
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2021-04-01 13:59:23 +0200

    frontend prise de rendez-vous

---
+++ components/disponibilite.php
@@ -0,0 +1,33 @@
+<div id="hot">
+        <div class="box">  
+                <div class="container">
+                    <div class="col-md-12">
+                        <h2>Nos disponiblité !</h2>
+                        <p> Selectionner le rendez-vous que vous désirer.</p>
+                    </div>    
+                        <form action="rdv.php" method="post">    
+                                    
+                                    <input type="radio" id="rdv1" name="rdv" value="vendredi 13 avril 2021 - 13h24" checked>
+                                    <label for="rdv1">vendredi 13 avril 2021 - 13h24</label>
+                                    <br>
+                                
+                                    <input type="radio" id="rdv2" name="rdv" >
+                                    <label for="rdv2">vendredi 13 avril 2021 - 14h24</label>
+                                    <br>
+                                
+                                    <input type="radio" id="rdv3" name="rdv" >
+                                    <label for="rdv3">samedi 14 avril 2021 - 13h24</label>
+                                    <br>
+                                
+                                </div>
+                                <div class=text-center>
+                                    <button type="submit" name="submit" class="btn btn-primary">
+                                    <i class="fa fa-calendar"></i> Prendre Rendez-Vous </button>
+                                </div>
+                                
+                        </form>
+
+                    
+                </div> 
+            </div>
+        </div> 
---
+++ components/sidebar_rdv.php
@@ -0,0 +1,5 @@
+<div class="panel panel-default">
+    <a href="rdv.php">
+        <img class="img-responsive" src="images/sidebar_rdv.jpg" alt="produit1">
+    </a>
+</div>
